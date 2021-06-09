c75e7e91783a29efeeac482c6ba298139c53b189
Sandip Chowdhury <sandipchowdhurycr7@gmail.com>
2021-06-09 13:23:30 +0530

    1st commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import Board from "./Board.jsx";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Board />
+  </React.StrictMode>,
+  document.getElementById("core")
+);
